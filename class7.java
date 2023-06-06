d4eb20d78edd02aa4f563c62183e7b0057ad8ac7
Joan-23 <132158006+Joan-23@users.noreply.github.com>
2023-06-06 06:24:13 +0530

    Add files via upload

---
+++ class7.java
@@ -0,0 +1,26 @@
+package project1;
+
+public class class7{
+
+	public static void main(String[] args) {
+	 int a,b,c;
+	 a=18*76;
+	 b=98*73;
+	 c=87*65;
+	 if(a>b && a>c)
+	 {
+		 System.out.println("A is greater");
+		 
+	 }
+	 else if(b>c)
+	 {
+		 System.out.println("B is greater");
+	 }
+	 else
+	 {
+		 System.out.println("C is greater");
+	 }
+
+	}
+
+}
