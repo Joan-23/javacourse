2fdebce1bfb3f6e1f615d71140b334761571c3d5
Joan-23 <132158006+Joan-23@users.noreply.github.com>
2023-06-06 06:23:56 +0530

    Add files via upload

---
+++ class6.java
@@ -0,0 +1,17 @@
+package project1;
+
+public class class6 {
+
+	public static void main(String[] args) {
+		
+		String a,b,c,d,e;
+		a="hi";
+		b="hey";
+		c="hello";
+		d="how is it going";
+		e="Wassupguys";
+		
+		System.out.println(a.charAt(0)+""+ b.charAt(0) + c.charAt(0) + d.charAt(0) + e.charAt(0) + a.charAt(a.length()-1) + b.charAt(b.length()-1) + c.charAt(c.length()-1) +d.charAt(d.length()-1)+ e.charAt(e.length()-1));
+	}
+
+}
