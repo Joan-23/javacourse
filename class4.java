5cc529e7e1ef21e35c2caa408288a31ee7e5cb56
Joan-23 <132158006+Joan-23@users.noreply.github.com>
2023-06-06 06:23:16 +0530

    Add files via upload

---
+++ class4.java
@@ -0,0 +1,15 @@
+package project1;
+
+public class class4 {
+
+	public static void main(String[] args) {
+		boolean a=true;
+		boolean b=false;
+		boolean c=true;
+		if((a||b)&&c)
+		{
+			System.out.println("Hello, World");
+		}
+	}
+
+}
