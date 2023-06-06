4673f2ccdc9fe92ce48d4053d085000427c1d43b
Joan-23 <132158006+Joan-23@users.noreply.github.com>
2023-06-06 06:22:09 +0530

    Add files via upload
    Class 3

---
+++ arithmetic.java
@@ -0,0 +1,17 @@
+package project1;
+
+public class arithmetic {
+
+	public static void main(String[] args) {
+ double a=12.8;
+ double b=16.5;
+ double c=22.3;
+ double d=89.7;
+ double e=62.834;
+  double avd;
+  avd=(a+b+c+d+e)/5;
+  System.out.println(avd);
+
+	}
+
+}
