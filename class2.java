eb23e4983fe9949c2fd23e4e21df178b13c63f2a
Joan-23 <132158006+Joan-23@users.noreply.github.com>
2023-06-06 06:21:25 +0530

    Add files via upload

---
+++ class2.java
@@ -0,0 +1,34 @@
+package project1;
+
+public class class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100;
+		long b=123456789L;
+		short c=12;
+		byte d=13;
+		double e=735726.0;
+		float f=123.0F;
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(a);
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(b);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(c);
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(d);
+		System.out.println(Double.MAX_EXPONENT);
+		System.out.println(Double.MIN_EXPONENT);
+		System.out.println(e);
+		System.out.println(Float.MIN_EXPONENT);
+		System.out.println(Float.MIN_EXPONENT);
+		System.out.println(f);
+	}
+
+}
