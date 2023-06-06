23bbde51248eabca7d584495cc1f97a59f801fc3
Joan-23 <132158006+Joan-23@users.noreply.github.com>
2023-06-06 06:24:28 +0530

    Add files via upload

---
+++ class8.java
@@ -0,0 +1,30 @@
+package project1;
+
+public class class9 {
+
+	public static void main(String[] args) {
+		
+		int a=31;
+		// is a divisible by 10 not exceeding 100
+
+		switch(a)
+		{
+		case 10:
+		case 20:
+		case 30:
+		case 40:
+		case 50:
+		case 60:
+		case 70:
+		case 80:
+		case 90:
+		case 100:
+			System.out.println("Divisible by 10");
+			break;
+			
+		default:
+			System.out.println("Number is not divisible by 10");
+		}
+	}
+
+}
